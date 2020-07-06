12760baacf621428ebcd2773478aa7a4bffe7132
Guilherme Enes <guilherme.epecci@gmail.com>
2020-07-06 18:07:39 -0300

    Memoria Interface Build

---
+++ TestingAppleTV/TestingAppleTV/Cells/MemoryCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemoryCell.swift
+//  TestingAppleTV
+//
+//  Created by Guilherme Enes on 06/07/20.
+//  Copyright © 2020 Guilherme Enes. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memoryImg: UIImageView!
+    
+}

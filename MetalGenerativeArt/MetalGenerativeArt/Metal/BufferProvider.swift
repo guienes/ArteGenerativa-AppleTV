79fc13a822adfc5af3ca37c77ac5fd299d2e7005
liakassardjian <liakassardjian@gmail.com>
2020-07-09 16:19:40 -0300

    Adicionado projeto com MTKView para gerar os fractais no iPhone

---
+++ MetalGenerativeArt/MetalGenerativeArt/Metal/BufferProvider.swift
@@ -0,0 +1,76 @@
+//
+//  BufferProvider.swift
+//  Model_IO_OSX
+//
+//  Created by Andriy K. on 6/20/15.
+//  Copyright © 2015 Andriy K. All rights reserved.
+//
+
+import UIKit
+import simd
+import Accelerate
+
+/// This class is responsible for providing a uniformBuffer which will be passed to vertex shader. It holds n buffers. In case n == 3 for frame0 it will give buffer0 for frame1 - buffer1 for frame2 - buffer2 for frame3 - buffer0 and so on. It's user responsibility to make sure that GPU is not using that buffer before use. For details refer to wwdc session 604 (18:00).
+
+struct Uniform {
+  var scale: Float = 1
+  var translation: (x: Float, y: Float) = (0, 0)
+  var maxNumberOfiterations: Float = 500
+  var aspectRatio: Float = 1
+  
+  fileprivate var raw: [Float] {
+    return [scale, translation.x, translation.y, maxNumberOfiterations, aspectRatio, 0, 0, 0]
+  }
+  
+  static var size = MemoryLayout<Float>.size * 8
+}
+
+
+/// I use class, to have deinit() for semaphores cleanup
+class BufferProvider {
+  
+  // General values
+  static let floatSize = MemoryLayout<Float>.size
+  static var bufferSize = Uniform.size
+  
+  // Reuse related
+  fileprivate(set) var indexOfAvaliableBuffer = 0
+  fileprivate(set) var numberOfInflightBuffers: Int
+  fileprivate var buffers:[MTLBuffer]
+  fileprivate(set) var avaliableResourcesSemaphore:DispatchSemaphore
+  
+  init(inFlightBuffers: Int, device: MTLDevice) {
+    
+    avaliableResourcesSemaphore = DispatchSemaphore(value: inFlightBuffers)
+    
+    numberOfInflightBuffers = inFlightBuffers
+    buffers = [MTLBuffer]()
+    
+    for _ in 0 ..< inFlightBuffers {
+      if let buffer = device.makeBuffer(length: BufferProvider.bufferSize, options: MTLResourceOptions()) {
+        buffer.label = "Uniform buffer"
+        buffers.append(buffer)
+      }
+    }
+  }
+  
+  deinit{
+    for _ in 0...numberOfInflightBuffers{
+      avaliableResourcesSemaphore.signal()
+    }
+  }
+  
+  func nextBufferWithData(_ uniform: Uniform) -> MTLBuffer {
+    
+    // Cycle through buffers
+    let uniformBuffer = self.buffers[indexOfAvaliableBuffer]
+    indexOfAvaliableBuffer += 1
+    if indexOfAvaliableBuffer == numberOfInflightBuffers {
+      indexOfAvaliableBuffer = 0
+    }
+    
+    memcpy(uniformBuffer.contents(), uniform.raw, Uniform.size)
+    return uniformBuffer
+  }
+  
+}
---
+++ MetalGenerativeArt/MetalGenerativeArt/Metal/Renderer.swift
@@ -0,0 +1,172 @@
+//
+//  Renderer.swift
+//  MetalGenerativeArt
+//
+//  Created by Lia Kassardjian on 09/07/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import MetalKit
+
+class Renderer: NSObject, MTKViewDelegate {
+    
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+    
+    var pipelineState: MTLRenderPipelineState!
+    var depthStencilState: MTLDepthStencilState!
+    var paletteTexture: MTLTexture!
+    var samplerState: MTLSamplerState!
+    var uniformBufferProvider: BufferProvider!
+    var mandelbrotSceneUniform = Uniform()
+    
+    fileprivate var square: Square!
+    
+    struct Constants {
+        var animatedBy: Float = 0.0
+    }
+    
+    var needsRedraw = true
+    var forceAlwaysDraw = false
+    var animate = false
+    var constants = Constants()
+    var time: Float = 0
+    
+    fileprivate var oldZoom: Float = 0.2
+    fileprivate var shiftX: Float = 0.5
+    fileprivate var shiftY: Float = 0
+    
+    init(device: MTLDevice, metalView: MTKView) {
+        self.device = device
+        commandQueue = device.makeCommandQueue()
+        square = Square(device: device)
+        
+        let textureLoader = MTKTextureLoader(device: device)
+        let path = Bundle.main.path(forResource: "pal", ofType: "png")!
+        let data = try! Data(contentsOf: URL(fileURLWithPath: path))
+        
+        paletteTexture = try! textureLoader.newTexture(data: data, options: nil)
+        samplerState = square.defaultSampler(device)
+        uniformBufferProvider = BufferProvider(inFlightBuffers: 3, device: device)
+        
+        super.init()
+        
+        buildPipelineState(metalView: metalView)
+        mandelbrotSceneUniform.aspectRatio = Float(metalView.frame.width / metalView.frame.height)
+    }
+    
+    private func buildPipelineState(metalView: MTKView) {
+        guard let library = device.makeDefaultLibrary(),
+            let vertexFunction = library.makeFunction(name: "vertexShader"),
+            let fragmentFunction = library.makeFunction(name: "fragmentShader")
+            else {
+            assert(false)
+            return
+        }
+        
+        let metalVertexDescriptor = getVertexDescriptor()
+        
+        pipelineState = compiledPipelineStateFrom(
+            vertexShader: vertexFunction,
+            fragmentShader: fragmentFunction,
+            vertexDescriptor: metalVertexDescriptor,
+            metalView: metalView
+        )
+        depthStencilState = compiledDepthState()
+    }
+    
+    func getVertexDescriptor() -> MTLVertexDescriptor {
+        let metalVertexDescriptor = MTLVertexDescriptor()
+        if let attribute = metalVertexDescriptor.attributes[0] {
+            attribute.format = MTLVertexFormat.float3
+            attribute.offset = 0
+            attribute.bufferIndex = 0
+        }
+        if let layout = metalVertexDescriptor.layouts[0] {
+            layout.stride = MemoryLayout<Float>.size * (3)
+        }
+        return metalVertexDescriptor
+    }
+    
+    func compiledPipelineStateFrom(vertexShader: MTLFunction,
+                                   fragmentShader: MTLFunction,
+                                   vertexDescriptor: MTLVertexDescriptor,
+                                   metalView: MTKView) -> MTLRenderPipelineState? {
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexDescriptor = vertexDescriptor
+        pipelineStateDescriptor.vertexFunction = vertexShader
+        pipelineStateDescriptor.fragmentFunction = fragmentShader
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        pipelineStateDescriptor.depthAttachmentPixelFormat = metalView.depthStencilPixelFormat
+        pipelineStateDescriptor.stencilAttachmentPixelFormat = metalView.depthStencilPixelFormat
+        
+        var compiledState: MTLRenderPipelineState?
+        do {
+            compiledState = try device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        return compiledState
+    }
+    
+    func compiledDepthState() -> MTLDepthStencilState {
+        let depthStencilDesc = MTLDepthStencilDescriptor()
+        depthStencilDesc.depthCompareFunction = MTLCompareFunction.less
+        depthStencilDesc.isDepthWriteEnabled = true
+        
+        return device.makeDepthStencilState(descriptor: depthStencilDesc)!
+    }
+    
+    
+    // MARK: - MTKViewDelegate
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        mandelbrotSceneUniform.aspectRatio = Float(size.width / size.height)
+        needsRedraw = true
+    }
+    
+    func draw(in view: MTKView) {
+        guard (needsRedraw == true || forceAlwaysDraw == true) else { return }
+        guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+        guard let drawable = view.currentDrawable else { return }
+        
+        if animate {
+            oldZoom += 0.001
+            shiftX += 0.0005
+        }
+        mandelbrotSceneUniform.translation = (shiftX, shiftY)
+        mandelbrotSceneUniform.scale = 1 / oldZoom
+        
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 1.0, green: 0.4, blue: 0.6, alpha: 1.0)
+        renderPassDescriptor.colorAttachments[0].storeAction = MTLStoreAction.store
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+            let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
+                return
+        }
+        
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setDepthStencilState(depthStencilState)
+        renderEncoder.setCullMode(MTLCullMode.none)
+        
+        if let squareBuffer = square?.vertexBuffer {
+            renderEncoder.setVertexBuffer(squareBuffer, offset: 0, index: 0)
+        }
+        
+        let uniformBuffer = uniformBufferProvider.nextBufferWithData(mandelbrotSceneUniform)
+        renderEncoder.setVertexBuffer(uniformBuffer, offset: 0, index: 1)
+        renderEncoder.setFragmentBuffer(uniformBuffer, offset: 0, index: 0)
+        
+        renderEncoder.setFragmentTexture(paletteTexture, index: 0)
+        renderEncoder.setFragmentSamplerState(samplerState, index: 0)
+        
+        renderEncoder.drawPrimitives(type: MTLPrimitiveType.triangle, vertexStart: 0, vertexCount: 6)
+        
+        renderEncoder.endEncoding()
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}

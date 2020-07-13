9623b48d2a946a0a17e51956f51b5ecca4c550a8
liakassardjian <liakassardjian@gmail.com>
2020-07-13 16:11:35 -0300

    Criação do modelo Memory

---
+++ TestingAppleTV/TestingAppleTV/Model/Memory.swift
@@ -0,0 +1,21 @@
+//
+//  Memory.swift
+//  TestingAppleTV
+//
+//  Created by Lia Kassardjian on 13/07/20.
+//  Copyright © 2020 Guilherme Enes. All rights reserved.
+//
+
+import UIKit
+
+class Memory {
+    
+    var set: Sets
+    var image: CGImage
+    
+    init(set: Sets, image: CGImage) {
+        self.set = set
+        self.image = image
+    }
+    
+}
